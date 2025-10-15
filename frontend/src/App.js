4f0e854b57b4dc74a3a472fe567ad05e87274b79
NitinBorase <nitinborase72@gmail.com>
2025-10-15 18:54:50 +0530

    Some change in App.js and Landing Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import './output.css';
+import './index.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import About from './pages/About';
